5d1b69b12504df01662d1ef32c69b2247862f300
Eddie Obrams <eddieobrams@gmail.com>
2023-07-26 00:40:08 +0100

    reset postcss config back to normal

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-const cssnano = require("cssnano");
-
 module.exports = {
-    plugins: [
-        "tailwindcss",
-        "autoprefixer",
-        cssnano({
-            preset: "default",
-        }),
-        purgecss({
-            content: ["./public/*.html"],
-            defaultExtractor: (content) =>
-                content.match(/[\w-/:]+(?<!:)/g) || [],
-        }),
-    ],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
