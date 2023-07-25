7131a9829e2f31ff792610b1e78ee5366d89f0cd
Eddie Obrams <eddieobrams@gmail.com>
2023-07-26 00:30:49 +0100

    try fix??? please

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        cssnano({
+            preset: "default",
+        }),
+        purgecss({
+            content: ["./public/*.html"],
+            defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+    ],
+};
