03930078384198a771ef1976a742bb54fdbb33ca
Eddie Obrams <eddieobrams@gmail.com>
2023-08-16 23:30:56 +0100

    lots of good progress, explore display dynamic now

--- components/ExploreDisplay.js
+++ components/ExploreDisplay.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 function Recipe(props) {
     return (
         <Link
-            href={`/recipes/${props.children}`}
+            href={`/recipes/${props.link}`}
             className=" shadow-2xl shadow-gray-600 hover:shadow-none hover:translate-y-3  transition-all p-4 w-[250px] max-h-[500px] bg-gradient-to-br from-gray-700 via-gray-800 to-gray-700 bg-opacity-50 rounded-lg mx-4 my-3 flex flex-col justify-center items-center text-white ${montserrat.className} "
         >
             <div
@@ -18,24 +18,26 @@ function Recipe(props) {
                     <Image
                         width={2000}
                         height={2000}
-                        alt={props.children}
-                        src={`/img/${props.img}`}
+                        alt={props.title}
+                        src={props.img}
                         className="object-cover shadow-lg hover:shadow-none transition-all h-[300px] rounded-lg border-8 border-opacity-60 border-gray-900"
                     />
                 </div>
 
                 <div className="RECIPEINFO">
                     <h1 className="RECIPETITLE text-center font-bold max-w-[200px] my-4 overflow-hidden whitespace-nowrap overflow-ellipsis ">
-                        {props.children}
+                        {props.title}
                     </h1>
                     <p className="RECIPEBOTTOM line-clamp-4 max-w-[200px] overflow-hidden whitespace-pre-line text-left max-h-[100px]">
                         {props.description}
                         By{" "}
                         <span className="font-semibold hover:underline">
-                            @eddieobrams
+                            {props.creator}
                         </span>
                         <br />
-                        Takes <span className="font-semibold">25</span> minutes
+                        Takes{" "}
+                        <span className="font-semibold">{props.time}</span>{" "}
+                        minutes
                     </p>
                 </div>
             </div>
@@ -43,17 +45,25 @@ function Recipe(props) {
     );
 }
 
-export default function ExploreDisplay() {
+export default function ExploreDisplay({ data }) {
     return (
         <div className="EXPLOREDISPLAY pb-[200px] pt-[20px] max-w-full h-full flex mx-[30px] justify-center mt-10 flex-wrap overflow-scroll shadow-inner">
-            <Recipe img="burger3.jpg">Gold Burger</Recipe>
-            <Recipe img="prawnsalad.jpg">Gingerbread House</Recipe>
-            <Recipe img="chicken1.jpg">Lemon & Herb Chicken Wings</Recipe>
-            <Recipe img="prawnsalad.jpg">Chicken Leg</Recipe>
-            <Recipe img="chicken3.jpg">Chicken Leg</Recipe>
-            <Recipe img="chicken3.jpg">Chicken Leg</Recipe>
-            <Recipe img="chicken3.jpg">Chicken Leg</Recipe>
-            <Recipe img="chicken3.jpg">Chicken Leg</Recipe>
+            {data.length >= 1 ? (
+                data.map((r) => {
+                    return (
+                        <Recipe
+                            key={r.recipeId}
+                            link={r.recipeId}
+                            img={r.image}
+                            time={r.time}
+                            creator={r.creator}
+                            title={r.title}
+                        />
+                    );
+                })
+            ) : (
+                <h1>No recipes found.</h1>
+            )}
         </div>
     );
 }
--- pages/explore.js
+++ pages/explore.js
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import ExploreDisplay from "@/components/ExploreDisplay";
 
-export default function Explore() {
+export default function Explore({ data }) {
     return (
         <Layout
             title="Munchbox - Explore."
@@ -10,7 +10,22 @@ export default function Explore() {
             <h1 className="absolute left-[50%] md:block hidden -translate-x-[50%] mt-1 font-extrabold text-gray-900 text-2xl underline underline-offset-1">
                 Recipe Explorer
             </h1>
-            <ExploreDisplay />
+            <ExploreDisplay data={data} />
         </Layout>
     );
+}
+
+export async function getServerSideProps() {
+    const response = await fetch("http://localhost:3000/api/recipes/", {
+        method: "GET",
+        headers: {
+            "munchbox-auth-key": [...JSON.parse(process.env["AUTH_KEY"])][0],
+        },
+    });
+    const data = await response.json();
+    return {
+        props: {
+            data: data.data,
+        },
+    };
 }
